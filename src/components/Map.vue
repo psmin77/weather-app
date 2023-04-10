5264221cf0e2b55f36c0212dd7f9be3668323da6
Seongmin <chr92@naver.com>
2023-04-10 16:32:20 +0900

    4-7. SCSS publishing

---
+++ src/components/Map.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="mapContainer">
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/scss/main.scss';
+
+#mapContainer {
+  @include center;
+  width: 100%;
+  height: 35%;
+  background-color: whitesmoke;
+
+  #map {
+    width: 80%;
+    height: 90%;
+    border-radius: 10px;
+  }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,35 @@
 <template>
-  <h1>Home.vue</h1>
+    <div class="wrap">
+        <div class="container">
+            <MainView />
+            <SubView />
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
+import MainView from '../components/MainView.vue';
+import SubView from '../components/SubView.vue';
 
-}
+export default {
+    components: { MainView, SubView }
+} 
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '~/scss/main.scss';
 
+.wrap {
+    @include center;
+    width: 100%;
+    height: 100vh;
+    font-size: 16px;
+}
+.container {
+    @include center-sb;
+    width: 55%;
+    height: 90%;
+    max-width: 1920px;
+    max-height: 1354px;
+}
 </style>
