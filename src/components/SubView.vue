882842c36b1933e84305fcfbaa00a35548f350c6
Seongmin <chr92@naver.com>
2023-04-12 14:07:05 +0900

    21. 마무리

--- src/components/SubView.vue
+++ src/components/SubView.vue
@@ -46,75 +46,78 @@
   <script>
   import Map from './Map.vue';
   import { computed, onMounted, ref, watchEffect } from "vue"; 
+  import { useStore } from 'vuex';
   import dayjs from "dayjs";
   import "dayjs/locale/ko";
-  import axios from 'axios';
   dayjs.locale("ko");
   
   export default {
-      components: { 
-          Map
-      },
-      setup() {
-          let currentTime = dayjs().format("YYYY. MM. DD. ddd");
-          let cityName = ref("");
-          let feeling = ref("");
-          let subWeatherData = ref([]);
+    components: { 
+        Map
+    },
+    setup() {
+        let currentTime = dayjs().format("YYYY. MM. DD. ddd");
+        let cityName = ref("");
+        let feeling = ref("");
+        let subWeatherData = ref([]);
   
-          // 타임스탬프로 변환
-          const Unix_timestamp = (dt) => {
-              let date = new Date(dt * 1000);
-              // padStart() 메서드는 현재 문자열의 시작을 다른 문자열로 채워, 주어진 길이를 만족하는 새로운 문자열을 반환합니다.
-              // 채워넣기는 대상 문자열의 시작(좌측)부터 적용됩니다.
-              let hour = date.getHours().toString().padStart(2, '0');
-              return hour.substring(-2) + '시';
-          };
+        // 타임스탬프로 변환
+        const Unix_timestamp = (dt) => {
+            let date = new Date(dt * 1000);
+            // padStart() 메서드는 현재 문자열의 시작을 다른 문자열로 채워, 주어진 길이를 만족하는 새로운 문자열을 반환합니다.
+            // 채워넣기는 대상 문자열의 시작(좌측)부터 적용됩니다.
+            let hour = date.getHours().toString().padStart(2, '0');
+            return hour.substring(-2) + '시';
+        };
+ 
+        const store = useStore();
+        const fetchOpenWeatherApi = async () => {
+            // API 호출을 위한 필수 데이터
+            try {
+                await store.dispatch('openWeatherApi/FETCH_OPENWEATHER_API');
+                const { currentFeelsLike, currentSunrise, currentSunset, currentVisibility } = store.state.openWeatherApi.currentWeather;
+
+                const isInitialCityName = store.state.openWeatherApi.cityName; // 초기 도시이름 데이터
+                // Composition API에서 AJAX요청과 데이터 변경을 하려면 데이터.value로 접근
+                cityName.value = isInitialCityName;
   
-          const fetchOpenWeatherApi = async () => {
-              // API 호출을 위한 필수 데이터
-              try {
-                  const API_KEY = process.env.VUE_APP_API_KEY;
-                  const resp = await axios.get(
-                      `https://api.openweathermap.org/data/2.5/forecast?id=1835848&appid=${API_KEY}&units=metric`
-                  );
+                let isFeelLikeTemp = computed(() => {
+                    return currentFeelsLike;
+                }); // 초기 체감온도 데이터
+                let isTimeOfSunrise = computed(() => {
+                    return currentSunrise;
+                }); // 일출시간 데이터
+                let isTimeOfSunSet = computed(() => {
+                    return currentSunset;
+                }); // 일몰시간 데이터
+                let isLineOfSight =  computed(() => {
+                    return currentVisibility;
+                }); // 가시거리 데이터let
   
-                  // 기준점에 따른 Array를 하나 만들고
-                  // 기준에 따른 메시지에 따른 Array를 하나 만들어서
-                  // 체감온도 데이터가 탐색을 해서
-                  // 원하는 값을 뽑는 로직으로 구성                
-                  let isInitialCityName = resp.data.city.name; // 초기 도시이름 데이터
-                  // Composition API에서 AJAX요청과 데이터 변경을 하려면 데이터.value로 접근
-                  cityName.value = isInitialCityName;
-  
-                  let isFeelLikeTemp = resp.data.list[0].main.feels_like; // 초기 체감온도 데이터
-                  let isTimeOfSunrise = resp.data.city.sunrise;// 일출시간 데이터
-                  let isTimeOfSunSet = resp.data.city.sunset;// 일몰시간 데이터
-                  let isLineOfSight =  resp.data.list[0].visibility;// 가시거리 데이터
-  
-                  // for...of 배열 순환 
-                  // for...in 객체 순환
-                  const pivots = [0, 10, 15, 20, 25, 30];
-                  const labels = ['매우 추움', '추움', '쌀쌀함', '선선함', '보통', '더움', '매우 더움'];
-                  let index = 0;
-                  for (const p of pivots) {
-                      if (isFeelLikeTemp <= p) break;
-                      index++;
-                  }
-                  feeling.value = labels[index];
-  
-                  // 가공한 데이터를 가지고 새로운 배열 생성
+                // for...of 배열 순환 
+                // for...in 객체 순환
+                const pivots = [0, 10, 15, 20, 25, 30];
+                const labels = ['매우 추움', '추움', '쌀쌀함', '선선함', '보통', '더움', '매우 더움'];
+                let index = 0;
+                for (const p of pivots) {
+                    if (isFeelLikeTemp.value <= p) break;
+                    index++;
+                }
+                feeling.value = labels[index];
+ 
+                // 가공한 데이터를 가지고 새로운 배열 생성
                   let isProcessedData = [
                       { 
                           name: '일출시간',
-                          value: Unix_timestamp(isTimeOfSunrise) 
+                          value: Unix_timestamp(isTimeOfSunrise.value) 
                       },
                       {
                           name: '일몰시간',
-                          value: Unix_timestamp(isTimeOfSunSet)
+                          value: Unix_timestamp(isTimeOfSunSet.value)
                       },
                       {
                           name: '가시거리',
-                          value: isLineOfSight.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',') + 'M',
+                          value: isLineOfSight.value.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',') + 'M',
                       },
                   ];
                   subWeatherData.value = isProcessedData;
